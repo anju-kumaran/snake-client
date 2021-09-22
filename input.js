3b62019bbfcb40390456072ffb5496db82733dc3
anju-kumaran <anju.naduth@gmail.com>
2021-09-22 19:27:17 -0400

    Created Input Module

--- input.js
+++ input.js
@@ -1,12 +1,4 @@
-const { connect } = require("./client");
-const { setupInput } = require("./input");
-
-console.log("Connecting ...");
-connect();
-
-setupInput();
-
-/*// setup interface to handle user input from stdin
+// setup interface to handle user input from stdin
 const stdin = process.stdin;
 
 const setupInput = function () {
@@ -28,4 +20,8 @@ const handleUserInput = function (key) {
   
 };
 
-setupInput();*/
+setupInput();
+
+module.exports = {
+  setupInput
+};
